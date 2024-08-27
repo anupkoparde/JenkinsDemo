7945b58bb5288d6e86ac9777f19d2ada057758b5
LENOVO <LENOVO@DESKTOP-KE1MN2L>
2024-08-27 21:40:33 +0530

    First Commit

---
+++ JenkinseDemo2/src/main/java/com/base/BaseClase.java
@@ -0,0 +1,34 @@
+package com.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClase {
+	WebDriver driver;	
+	@BeforeMethod
+	public void launch() {
+		
+	if(System.getProperty("browser").equalsIgnoreCase("Chrome")) {
+		WebDriverManager.chromedriver().setup();
+	driver= new ChromeDriver();
+	}
+	else if(System.getProperty("browser").equalsIgnoreCase("Edge")) {
+		WebDriverManager.edgedriver().setup();
+		driver= new EdgeDriver();
+	}
+	driver.manage().window().maximize();
+	driver.get("https://www.google.co.in/");
+	
+	}
+  @AfterMethod
+  public void terdown() {
+	  
+	 driver.close();
+	  
+  }
+}
---
+++ JenkinseDemo2/src/test/java/com/test/Testcase.java
@@ -0,0 +1,35 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class Testcase {
+
+	@Test
+	public void testcase1() {
+		System.out.println("this s testcase1");
+	}
+	
+	@Test
+	public void testcase2() {
+		System.out.println("this s testcase2");
+	}
+	
+	@Test
+	public void testcase3() {
+		System.out.println("this s testcase3");
+	}
+	
+	
+	@Test
+	public void testcase4() {
+		System.out.println("this s testcase4");
+	}
+	
+	@Test
+	public void testcase5() {
+		System.out.println("this s testcase5");
+	}
+	
+
+}
+
